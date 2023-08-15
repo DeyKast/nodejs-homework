8941750d060ca519bb2018b3317a81f1bfc895c9
DeyKast <deykast@gmail.com>
2023-08-15 11:40:47 +0300

    complete-HM-04

--- schemas/index.js
+++ schemas/index.js
@@ -1,2 +1,6 @@
 export { default as contactSchema } from "./contactsSchema.js";
 export { default as contactUpdateFavoriteSchema } from "./contactsSchema.js";
+
+export { default as userSignupSchema } from "../schemas/userSchema.js";
+export { default as userSigninSchema } from "../schemas/userSchema.js";
+export { default as updateSubscription } from "../schemas/userSchema.js";
