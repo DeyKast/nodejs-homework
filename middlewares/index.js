98a180063050bc6cb78408b1523d75c223d245bf
DeyKast <deykast@gmail.com>
2023-08-18 14:04:14 +0300

    Complete-HM-05

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,3 +2,4 @@ export { default as validateBody } from "./validateBody.js";
 export { default as validateObject } from "./validateObject.js";
 export { default as isValidId } from "./isValidId.js";
 export { default as authenticate } from "./authenticate.js";
+export { default as upload } from "./upload.js";
--- models/userModel.js
+++ models/userModel.js
@@ -5,23 +5,28 @@ const userSchema = new Schema(
   {
     password: {
       type: String,
-      required: [true, 'Set password for user'],
+      required: [true, "Set password for user"],
     },
     email: {
       type: String,
-      required: [true, 'Email is required'],
+      required: [true, "Email is required"],
       unique: true,
     },
     subscription: {
       type: String,
       enum: ["starter", "pro", "business"],
-      default: "starter"
+      default: "starter",
     },
     token: {
       type: String,
-    }
-  }, { versionKey: false }
-)
+    },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
 
 userSchema.pre("findOneAndUpdate", handleUpdateValidate);
 
